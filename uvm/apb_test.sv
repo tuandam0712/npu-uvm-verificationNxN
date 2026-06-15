0b80835ad424967b2692eccbf2a794a3c5399021
tuandam0712 <tuandp.23ce@vku..udn.vn>
2026-06-15 12:45:01 +0700

    update sparse

--- uvm/apb_test.sv
+++ uvm/apb_test.sv
@@ -18,6 +18,7 @@ class apb_test extends uvm_test;
         apb_reg_access_sequence seq;
         apb_matrix_compute_sequence compute_seq;
         apb_zero_matrix_sequence zero_seq;
+        apb_sparse_matrix_sequence sparse_seq;
         phase.raise_objection(this);
 
         seq = apb_reg_access_sequence::type_id::create("seq");
@@ -26,6 +27,8 @@ class apb_test extends uvm_test;
         compute_seq.start(env.agent.sqr);
         zero_seq = apb_zero_matrix_sequence::type_id::create("zero_seq");
         zero_seq.start(env.agent.sqr);
+        sparse_seq = apb_sparse_matrix_sequence::type_id::create("sparse_seq");
+        sparse_seq.start(env.agent.sqr);
         phase.drop_objection(this);
     endtask
 
